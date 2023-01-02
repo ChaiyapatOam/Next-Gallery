9d9b57929963a4ccd157828a1c53093e16d2d125
Chaiyapat Meeying <chaiyapatoam12@gmail.com>
2023-01-02 16:43:15 +0700

    :sparkles:feat: handle Loading

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,15 +7,30 @@ import { useRouter } from "next/router";
 import Loading from "../components/Loading";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const [isLoading, setIsLoading] = useState(true);
-  const router = useRouter();
+  const [loading, setLoading] = useState(false);
+  const Router = useRouter();
   useEffect(() => {
-    router.isReady && setIsLoading(false);
+    const start = () => {
+      // console.log("start");
+      setLoading(true);
+    };
+    const end = () => {
+      // console.log("finished");
+      setLoading(false);
+    };
+    Router.events.on("routeChangeStart", start);
+    Router.events.on("routeChangeComplete", end);
+    Router.events.on("routeChangeError", end);
+    return () => {
+      Router.events.off("routeChangeStart", start);
+      Router.events.off("routeChangeComplete", end);
+      Router.events.off("routeChangeError", end);
+    };
   }, []);
   return (
     <div>
-      {isLoading ? (
-       <Loading/>
+      {loading ? (
+        <Loading />
       ) : (
         <div>
           <Head>
